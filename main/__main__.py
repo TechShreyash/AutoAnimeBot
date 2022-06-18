1493d7bd5024d44dffd33ddf0aaf2c67d8913214
TechShreyash <shreyashdheemar123@gmail.com>
2022-06-18 18:59:23 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -11,13 +11,16 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, 
 async def start(bot, message: Message):
   await message.reply_text("I am working fine :)")
 
+async def start_():
+  asyncio.create_task(auto_parser())
+
 if __name__ == "__main__":
   print("==================================")
   print("[INFO]: BOT STARTED BOT SUCCESSFULLY")
   print("==========JOIN @TECHZBOTS=========")
 
   print("Creating Parse task")
-  asyncio.run(asyncio.create_task(auto_parser()))
+  asyncio.run(start_())
 
   app.send_message(CHANNEL_ID,text="Bot Started")
   app.send_video("downloads/mp4.mp4")
