5a8cc4b8ea2ed69162aaeb0a68edd3149847a76f
TechShreyash <shreyashdheemar123@gmail.com>
2022-06-18 11:57:57 +0530

    Update uploader.py

--- main/modules/uploader.py
+++ main/modules/uploader.py
@@ -7,25 +7,28 @@ import time
 from main import app
 
 async def upload_video(msg: Message,file):
-    fuk = isfile(file)
-    if fuk:
-        r = msg
-        c_time = time.time()
-        z = await r.reply_video(
-        video=file,
-        caption=os.path.basename(file),
-        progress=progress_for_pyrogram,
-        progress_args=(
-            os.path.basename(file),
-            r,
-            c_time
-        )
-        )
-        os.remove(file)
-    
     try:
-        await r.delete()
-        os.remove(file)
-    except:
-        pass
+        fuk = isfile(file)
+        if fuk:
+            r = msg
+            c_time = time.time()
+            z = await r.reply_video(
+            video=file,
+            caption=os.path.basename(file),
+            progress=progress_for_pyrogram,
+            progress_args=(
+                os.path.basename(file),
+                r,
+                c_time
+            )
+            )
+            os.remove(file)
+        
+        try:
+            await r.delete()
+            os.remove(file)
+        except:
+            pass
+    except Exception as e:
+        print(e)
     return
