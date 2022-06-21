448e4cf771e4ca566bf7996b662c4abdc5dbc2b4
TechShreyash <shreyashdheemar123@gmail.com>
2022-06-21 22:52:49 +0530

    .

--- main/modules/uploader.py
+++ main/modules/uploader.py
@@ -11,49 +11,44 @@ import time
 from main import app
 
 async def upload_video(msg: Message,file,id,tit,name,message_id,ttl):
+    
+    fuk = isfile(file)
+    if fuk:
+        r = msg
+        c_time = time.time()
+        duration = get_duration(file)
+        size = get_filesize(file)
+        ep_num = get_epnum(name)
+        thumbnail,w,h = generate_thumbnail(id,file,tit,ep_num,size,format_time(duration))
+        
+        buttons = InlineKeyboardMarkup([
+            [
+                InlineKeyboardButton(text="Info", url="https://t.me/Anime_Dex"),
+                InlineKeyboardButton(text="Comments", url=f"https://t.me/AniDec/{message_id}?thread={message_id}")
+            ]
+        ])
+        caption = f"🎥 **{name}**"
+        x = await app.send_video(
+            CHANNEL_ID,
+        file,
+        caption=caption,
+        duration=duration,
+        width=w,
+        height=h,
+        thumb=thumbnail,
+        reply_markup=buttons,
+        file_name=os.path.basename(file),
+        progress=progress_for_pyrogram,
+        progress_args=(
+            os.path.basename(file),
+            r,
+            c_time,
+            ttl
+        )
+        )        
     try:
-        fuk = isfile(file)
-        if fuk:
-            r = msg
-            c_time = time.time()
-
-            duration = get_duration(file)
-            size = get_filesize(file)
-            ep_num = get_epnum(name)
-            thumbnail,w,h = generate_thumbnail(id,file,tit,ep_num,size,format_time(duration))
-            
-            buttons = InlineKeyboardMarkup([
-                [
-                    InlineKeyboardButton(text="Info", url="https://t.me/Anime_Dex"),
-                    InlineKeyboardButton(text="Comments", url=f"https://t.me/AniDec/{message_id}?thread={message_id}")
-                ]
-            ])
-
-            caption = f"🎥 **{name}**"
-
-            x = await app.send_video(
-                CHANNEL_ID,
-            file,
-            caption=caption,
-            duration=duration,
-            width=w,
-            height=h,
-            thumb=thumbnail,
-            reply_markup=buttons,
-            file_name=os.path.basename(file),
-            progress=progress_for_pyrogram,
-            progress_args=(
-                os.path.basename(file),
-                r,
-                c_time,
-                ttl
-            )
-            )        
-        try:
-            await r.delete()
-            os.remove(file)
-        except:
-            pass
-    except Exception as e:
-        print("In uploader.py", e)
+        await r.delete()
+        os.remove(file)
+    except:
+        pass
     return
