2906207f1c7e57098942c62f55f9b786b574df43
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2022-06-22 13:52:32 +0530

    Delete start.py

--- main/modules/start.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from main.modules.tg_handler import tg_handler
-
-is_on = 0
-
-async def start_hand():
-    if is_on == 0:
-        asyncio.create_task(tg_handler())
-        is_on = 1
