c1277f54863f82fe6070a1b2ea0e01be29640f13
TechShreyash <shreyashdheemar123@gmail.com>
2022-07-06 14:52:40 +0530

    Update compressor.py

--- main/modules/compressor.py
+++ main/modules/compressor.py
@@ -38,7 +38,7 @@ async def gg():
     out,
     "-y"      
   ]
-  cmd = 'ffmpeg -hide_banner -loglevel quiet -progress "progressaa.txt" -i "video.mkv" -preset fast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "out.mkv" -y'
+  #cmd = 'ffmpeg -hide_banner -loglevel quiet -progress "progressaa.txt" -i "video.mkv" -preset fast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "out.mkv" -y'
   
   subprocess.Popen(cmd,shell=True)
   print("omfo")
