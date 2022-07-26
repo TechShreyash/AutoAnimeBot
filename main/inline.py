593609a4addd907dc8f1726fd3c8e4719f88694b
Vedant VN <81044922+Vedant-VN@users.noreply.github.com>
2022-07-26 18:07:58 +0530

    hm

--- main/inline.py
+++ main/inline.py
@@ -4,11 +4,17 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 button1 = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton(text="View Schedule", url= f"https://t.me/{UPLOADS_USERNAME}/{SCHEDULE_ID}")
+                InlineKeyboardButton(text="❤️‍ Anime Schedule ❤️‍", url= f"https://t.me/{UPLOADS_USERNAME}/{SCHEDULE_ID}")
             ],
-            [
-                InlineKeyboardButton(text="Index Channel", url= f"https://t.me/{INDEX_USERNAME}"),
-                InlineKeyboardButton(text="Discussion Group", url= f"https://t.me/+Q0xUxmKd1bQyMWI9")
+           
+                [
+                InlineKeyboardButton(text="🔰 Index", url= f"https://t.me/{INDEX_USERNAME}"),
+                InlineKeyboardButton(text="⚡️ Network ⚡️", url= f"https://t.me/AnimeSigma_Network"),
+                InlineKeyboardButton(text="💠 Group", url= f"https://t.me/Anime_Sigma")
+            ],
+               [
+                       
+                InlineKeyboardButton(text="➤ Our Main Channel", url= f"https://t.me/AnimeSigma")
             ]
         ]
     )
@@ -16,7 +22,16 @@ button1 = InlineKeyboardMarkup(
 button2 = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton(text="Check Queue", url= f"https://t.me/{UPLOADS_USERNAME}/{STATUS_ID}")
-            ]
+                InlineKeyboardButton(text="Queued List ✔", url= f"https://t.me/{UPLOADS_USERNAME}/{STATUS_ID}")
+            ],
+               
+                 [
+                InlineKeyboardButton(text="Owner 👀", url= f"https://t.me/Vedant_Vn"),
+                InlineKeyboardButton(text="Ongoing Sigma✨", url= f"https://t.me/Anime_List_Index_Sigma/210")
+                 ],
+                
+               [        
+                InlineKeyboardButton(text="➤ Our Anime Group", url= f"https://t.me/Anime_Sigma")
+            ],
         ]
     )
